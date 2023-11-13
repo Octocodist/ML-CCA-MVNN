2111ae3b924f04ff4411b27ca25c1e83e8e01fd9
Felix Illes <felix@ethz.cethz.ch>
2023-11-13 13:55:43 +0100

    added seed to world

--- compare-models/dataset_generation/dataset.py
+++ compare-models/dataset_generation/dataset.py
@@ -44,14 +44,14 @@ def generate_all_bundle_value_pairs(world, k, mode):
 
 
 
-        bundle_space = [np.random.choice([0, 1], len(M)) for _ in range(k)]
-        # Only use unique samples.
-        bundle_space = np.unique(np.array(bundle_space), axis=0)
-        print("Num Unique Samples: ", len(bundle_space))
+       bundle_space = [np.random.choice([0, 1], len(M)) for _ in range(k)]
+       # Only use unique samples.
+       bundle_space = np.unique(np.array(bundle_space), axis=0)
+       print("Num Unique Samples: ", len(bundle_space))
     else:
-        #this creates a list of lists of length len(M) with all possible combinations of 0 and 1
-        #this calculates the cartesian product of the list [0,1] with itself len(M) times
-        bundle_space = list(itertools.product([0, 1], repeat=len(M)))
+       #this creates a list of lists of length len(M) with all possible combinations of 0 and 1
+       #this calculates the cartesian product of the list [0,1] with itself len(M) times
+       bundle_space = list(itertools.product([0, 1], repeat=len(M)))
 
     #bundle_value_pairs = np.array(
     #    [list(x) + [world.calculate_value(bidder_id, x) for bidder_id in N] for x in tqdm(bundle_space)])
@@ -64,7 +64,7 @@ def init_parser():
     parser.add_argument('-m','--mode',  type=str, help='Choose mode to sample from mrvm, srvm', choices=['mrvm','srvm','gsvm','lsvm'], default='gsvm')
     parser.add_argument('-b','--bidder_id',nargs=1, type=int, help='Define bidder id', default=[1])
     parser.add_argument('-n','--num_bids',nargs=1 ,type=int, help='Define bidder id', default=[250])
-    parser.add_argument('-sd','--seed',nargs=1 ,type=int, help='Define seed', default=1 )
+    parser.add_argument('-sd','--seed',nargs=1 ,type=int, help='Define seed', default=1234 )
     parser.add_argument('-s','--save',action='store_true', help='Save the generated dataset', default=False)
     parser.add_argument('-p','--print', action= 'store_true', help='Print the generated dataset', default=False)
     parser.add_argument('-num', '--number_of_instances', type=int, default=1, help='Num. training data  (1)')
@@ -95,10 +95,7 @@ def main():
     print("Current mode :"+ str(args.mode))
     # create an instance
     #domain = getattr(PySats.getInstance(),"create_"+str(args.mode))(seed=1)
-    if args.mode == "srvm":
-        domain = PySats.getInstance().create_srvm()
-    else:    
-        domain = getattr(PySats.getInstance(),"create_"+str(args.mode))()
+    domain = getattr(PySats.getInstance(),"create_"+str(args.mode))(seed=args.seed[0])
     print("FOUND DOMAIN")
     # use the GenericWrapper which uses goods with multiple items per good
     # a bundle is not anymore a binary vector but a vector of integers
